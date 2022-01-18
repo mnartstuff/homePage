9fcdb85debc89f9302a9729043058efcc07d0ff5
mnartstuff <alexgeerartist@gmail.com>
2022-01-18 17:09:18 -0600

    new, modular design

--- scripts.js
+++ scripts.js
@@ -1,16 +1,38 @@
 (function(){
-    "use strict";
 
-    const links = document.querySelectorAll('a');
+  const mainLinks = document.querySelectorAll('.main-link');
+  const modal = document.querySelector('.modal');
+  const modalMenu = document.querySelector('.modal-menu');
 
-    for( let i=0; i<links.length; i++ ){
+  for(const link of mainLinks) {
+    link.addEventListener('click', () => {
+      for(const all of mainLinks) {
+        all.classList.add('faded');
+      }
+      modal.classList.add('show-modal');
+  const thisClass = event.srcElement.id;
+  const theseLinks = document.querySelectorAll(`.${thisClass}`);
+  for (const eachLink of theseLinks) {
+    eachLink.classList.add('show-tile');
+  }
+    });
+  }
 
-        links[i].setAttribute("target", "_blank");
-    }
+  modal.addEventListener('click', (e) => {
 
+    const tiles = document.querySelectorAll('.tile');
+    if (e.target !== e.currentTarget)
+    return;
 
+      modal.classList.remove('show-modal');
 
+      for (const tile of tiles) {
+        tile.classList.remove('show-tile');
+      }
+      for (const link of mainLinks) {
+        link.classList.remove('faded');
+      }
+
+  });
+  
 }());
-
-
-// setAttribute("target", "_blank");
